8dfdb3af205307ede05221b5dba2525e25011255
hisham nawzer <hishamnawzer@gmail.com>
2020-09-06 22:51:14 +0900

    new component created

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Media }from 'reactstrap'
+function Menu() {
+
+    const menu;
+  return (
+    <div className="container">
+      <div className="row">
+            <Media list>
+                {menu}
+            </Media>
+      </div>
+    </div>
+  );
+}
+
+export default Menu;
