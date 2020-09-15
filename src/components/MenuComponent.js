e5dca526a1760c44cdd61e2bdf162a8f5849c4ba
hisham nawzer <hishamnawzer@gmail.com>
2020-09-16 07:38:35 +0900

    added click event

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,34 +1,35 @@
 import React from "react";
-import {Media }from 'reactstrap'
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
 function Menu(props) {
+  //  const dishes=
 
+  function onDishSelect(dish) {
+    console.log(dish);
+  }
+  const menu = props.dishes.map((dish) => {
+    return (
+      <div key={dish.id} className="col-12 col-md-5 m-1 ">
+        <Card onClick={() => onDishSelect(dish)}>
+          <CardImg width="100%" object src={dish.image} alt={dish.name} />
 
-  //  const dishes= 
-    
-  
-    const menu=props.dishes.map((dish)=>{
-      return(
-        <div key={dish.id} className="col-12 mt-5">
-          <Media tag="li">
-            <Media left middle>
-              <Media object src={dish.image }alt={dish.name}/>
-               </Media>
-               <Media body className="ml-5">
-                 <Media heading>{dish.name} </Media>
-                 <p>{dish.description}</p>
-               </Media>
-          </Media>
-        </div>
-      );
-    });
+          <CardImgOverlay>
+            <CardTitle>{dish.name} </CardTitle>
+          </CardImgOverlay>
+        </Card>
+      </div>
+    );
+  });
   return (
     <div className="container">
-      <div className="row">
-            <Media list>
-                {menu}
-            </Media>
-      </div>
+      <div className="row">{menu}</div>
     </div>
   );
-  }
+}
 export default Menu;
