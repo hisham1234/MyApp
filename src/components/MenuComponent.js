b1398ceb663395f653e4bf10dd619f6e9dad47c2
hisham30 <hishamtshirt@gmail.com>
2020-09-18 14:30:51 +0900

    done with components

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import {
   Card,
   CardImg,
@@ -9,10 +9,31 @@ import {
 } from "reactstrap";
 function Menu(props) {
   //  const dishes=
+  const [selectedDish, setSelectedDish] = useState(null);
 
-  function onDishSelect(dish) {
-    console.log(dish);
+  const onDishSelect = (dish) => {
+    debugger;
+    console.log("came");
+    setSelectedDish(dish);
+    console.log(selectedDish);
   }
+
+  function renderDish(dish) {
+    if (dish != null)
+        return(
+            <Card>
+                <CardImg top src={dish.image} alt={dish.name} />
+                <CardBody>
+                  <CardTitle>{dish.name}</CardTitle>
+                  <CardText>{dish.description}</CardText>
+                </CardBody>
+            </Card>
+        );
+    else
+        return(
+            <div></div>
+        );
+}
   const menu = props.dishes.map((dish) => {
     return (
       <div key={dish.id} className="col-12 col-md-5 m-1 ">
@@ -29,6 +50,11 @@ function Menu(props) {
   return (
     <div className="container">
       <div className="row">{menu}</div>
+      <div className="row">
+                  <div  className="col-12 col-md-5 m-1">
+                    {renderDish(selectedDish)}
+                  </div>
+                </div>
     </div>
   );
 }
